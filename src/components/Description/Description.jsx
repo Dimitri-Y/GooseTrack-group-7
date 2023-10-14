700cba3a1f930e953ce079370ae3626f486c5857
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-10-13 18:22:33 -0600

    update main page
    Finished description section, start reviews section.

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -19,8 +19,22 @@ import Calendar2xTabPng from '../../images/mainPage/tablet/tablet_calendar@2x.pn
 import Calendar1xDeskPng from '../../images/mainPage/desktop/desktop_calendar@1x.png';
 import Calendar2xDeskPng from '../../images/mainPage/desktop/desktop_calendar@2x.png';
 
+import Sidebar1xMobPng from '../../images/mainPage/mobile/mobile_sidebar@1x.png';
+import Sidebar2xMobPng from '../../images/mainPage/mobile/mobile_sidebar@2x.png';
+import Sidebar1xTabPng from '../../images/mainPage/tablet/tablet_sidebar@1x.png';
+import Sidebar2xTabPng from '../../images/mainPage/tablet/tablet_sidebar@2x.png';
+import Sidebar1xDeskPng from '../../images/mainPage/desktop/desktop_sidebar@1x.png';
+import Sidebar2xDeskPng from '../../images/mainPage/desktop/desktop_sidebar@2x.png';
+
+import All1xMobPng from '../../images/mainPage/mobile/mobile_all@1x.png';
+import All2xMobPng from '../../images/mainPage/mobile/mobile_all@2x.png';
+import All1xTabPng from '../../images/mainPage/tablet/tablet_all@1xTab.png';
+import All2xTabPng from '../../images/mainPage/tablet/tablet_all@2xTab.png';
+import All1xDeskPng from '../../images/mainPage/desktop/desktop_all@1x.png';
+import All2xDeskPng from '../../images/mainPage/desktop/desktop_all@2x.png';
+
 export const Description = () => {
-  const { isRetina, isMobile, isTablet, isDesktop } = useAdaptivePicture();
+  const { isMobile, isTablet, isDesktop } = useAdaptivePicture();
 
   return (
     <DescriptionWrapper>
@@ -61,6 +75,85 @@ export const Description = () => {
               />
 
               <Image src={`${Calendar1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+        </ImageWrapper>
+      </DescriptionSection>
+      <DescriptionSection>
+        <DescriptionSectionBox>
+          <Number>2.</Number>
+          <Title>SIDEBAR</Title>
+          <SubTitle>VIEW</SubTitle>
+          <Text>
+            GooseTrack offers easy access to your account settings, calendar,
+            and filters. The "My Account" section allows you to manage your
+            profile information and preferences, while the calendar provides a
+            quick and convenient way to view your upcoming events and tasks.
+          </Text>
+        </DescriptionSectionBox>
+        <ImageWrapper>
+          {isMobile && (
+            <picture>
+              <source srcSet={`${Sidebar1xMobPng} 1x, ${Sidebar2xMobPng} 2x`} />
+              <Image src={`${Sidebar1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+          {isTablet && (
+            <picture>
+              <source
+                srcSet={`${Sidebar1xTabPng} 1x, ${Sidebar2xTabPng} 2x`}
+                media="(min-width: 768px)"
+              />
+              <Image src={`${Sidebar1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+          {isDesktop && (
+            <picture>
+              <source
+                srcSet={`${Sidebar1xDeskPng} 1x, ${Sidebar2xDeskPng} 2x`}
+                media="(min-width: 1280px)"
+              />
+
+              <Image src={`${Sidebar1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+        </ImageWrapper>
+      </DescriptionSection>
+      <DescriptionSection>
+        <DescriptionSectionBox>
+          <Number>3.</Number>
+          <Title>ALL IN</Title>
+          <SubTitle>ONE</SubTitle>
+          <Text>
+            GooseTrack is an all-in-one productivity tool that helps you stay on
+            top of your tasks, events, and deadlines. Say goodbye to scattered
+            to-do lists and hello to streamlined productivity with GooseTrack.
+          </Text>
+        </DescriptionSectionBox>
+        <ImageWrapper>
+          {isMobile && (
+            <picture>
+              <source srcSet={`${All1xMobPng} 1x, ${All2xMobPng} 2x`} />
+              <Image src={`${All1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+          {isTablet && (
+            <picture>
+              <source
+                srcSet={`${All1xTabPng} 1x, ${All2xTabPng} 2x`}
+                media="(min-width: 768px)"
+              />
+              <Image src={`${All1xMobPng}`} alt="Calendar" />
+            </picture>
+          )}
+          {isDesktop && (
+            <picture>
+              <source
+                srcSet={`${All1xDeskPng} 1x, ${All2xDeskPng} 2x`}
+                media="(min-width: 1280px)"
+              />
+
+              <Image src={`${All1xMobPng}`} alt="Calendar" />
             </picture>
           )}
         </ImageWrapper>
--- src/components/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/ReviewsSlider/ReviewsSlider.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import SwiperCore, { Autoplay } from 'swiper';
+// import { Navigation } from 'swiper';
+// import 'swiper/css';
+// import 'swiper/css/virtual';
+// import 'swiper/css/navigation';
+import { ReactComponent as ArrowLeft } from '../../images/svg/arrowBack.svg';
+import { ReactComponent as ArrowRight } from '../../images/svg/arrowNext.svg';
+import {
+  ReviewsSection,
+  ReviewsTitle,
+  SliderWrapper,
+  SwiperNavBox,
+  SwiperNavBtn,
+} from './ReviewsSlider.styed';
+
 const ReviewsSlider = () => {
-    return (
-      <>
-        <h2>ReviewsSlider</h2>
-      </>
-    );
+  return (
+    <ReviewsSection>
+      <ReviewsTitle>REVIEWS</ReviewsTitle>
+      <SliderWrapper>
+        <Swiper>Here will bi slides</Swiper>
+      </SliderWrapper>
+      <SwiperNavBox>
+        <SwiperNavBtn id="my-prev-button">
+          <ArrowLeft />
+        </SwiperNavBtn>
+        <SwiperNavBtn id="my-next-button">
+          <ArrowRight />
+        </SwiperNavBtn>
+      </SwiperNavBox>
+    </ReviewsSection>
+  );
 };
 
 export default ReviewsSlider;
