27780cc5a621dedc9dfe74814df09dfc2e7470bf
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-10-14 03:00:01 +0300

    Create file architecture for MainLayout

---
+++ src/components/MainLayout/UserInfo/UserInfo.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const UserInfoStyled = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const UserName = styled.img`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 1.28;
+  color: #343434;
+`;
+
+export const UserAvatar = styled.span`
+  width: 32px;
+  height: 32px;
+  border-radius: 32px;
+  border: 1.8px solid #3E85F3;
+`;
