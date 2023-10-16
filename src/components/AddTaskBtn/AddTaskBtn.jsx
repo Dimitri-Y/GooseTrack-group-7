eeda50d4105263f674c22521d24b23390de47cd8
Kateryna1706 <119225233+Kateryna1706@users.noreply.github.com>
2023-10-16 22:35:22 +0200

    added scroll to tasks Feature/choosed day(#19)

--- src/components/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/AddTaskBtn/AddTaskBtn.jsx
@@ -1,5 +1,22 @@
-const AddTaskBtn = () => {
-  console.log();
-  return <button type="button">Add task</button>;
+import { useState } from 'react';
+import TaskModal from '../TaskModal/TaskModal';
+import { Button } from './AddTaskBtn.styled';
+
+const AddTaskBtn = ({ header }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleClick = () => {
+    console.log(`Click button add task to ${header}`);
+    setIsOpenModal(true);
+  };
+
+  return (
+    <>
+      <Button type="button" onClick={handleClick}>
+        Add task
+      </Button>
+      {isOpenModal && <TaskModal headerCategory={header} />}
+    </>
+  );
 };
 export default AddTaskBtn;
