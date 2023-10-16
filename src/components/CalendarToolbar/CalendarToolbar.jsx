2546f704da0f86515dd6dcd52684ede1790fde33
Dimitri-Y <dimoon.bond@gmail.com>
2023-10-17 00:12:02 +0300

    renamed Hesh

--- src/components/CalendarToolbar/CalendarToolbar.jsx
+++ src/components/CalendarToolbar/CalendarToolbar.jsx
@@ -2,13 +2,13 @@ import PeriodPaginator from 'components/PeriodPaginator/PeriodPaginator';
 import PeriodTypeSelect from 'components/PeriodTypeSelect/PeriodTypeSelect';
 
 const CalendarToolbar = () => {
-    return (
-        <div>
-        <a>CalendarToolbar</a>
-        <PeriodPaginator />
-        <PeriodTypeSelect />
-        </div>
-    );
-}
+  return (
+    <div>
+      <a>Calendar</a>
+      <PeriodPaginator />
+      <PeriodTypeSelect />
+    </div>
+  );
+};
 
 export default CalendarToolbar;
