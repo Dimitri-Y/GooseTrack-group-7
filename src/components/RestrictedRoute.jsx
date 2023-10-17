f77714ccba168526610ce9bef4689b28ec416810
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-10-17 23:32:59 +0300

    Create public and private routes

---
+++ src/components/RestrictedRoute.jsx
@@ -0,0 +1,9 @@
+// import { useSelector } from 'react-redux';
+// import { selectIsLoggedIn } from '../redux/auth/selectors';
+import { Navigate } from 'react-router-dom';
+const RestrictedRoute = ({ component: Component, redirectTo = '/calendar' }) => {
+    const isLoggedIn = true;//useSelector(selectIsLoggedIn);
+    return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+export default RestrictedRoute;
