39c3835cacce71cf51f0a542fde260e38e6e3551
Katerina <110998098+KaterynaNikitina@users.noreply.github.com>
2023-10-18 04:44:01 -0600

    Fbmodal (#34)
    * fbmodal edited
    
    * feedback button and modal added

--- src/components/MainLayout/AddFeedbackBtn/AddFeedbackBtn.styled.jsx
+++ src/components/MainLayout/AddFeedbackBtn/AddFeedbackBtn.styled.jsx
@@ -1,5 +1,31 @@
+
 import styled from 'styled-components';
 
 export const FeedbackBtn = styled.button`
+  font-family: "Inter 600", sans-serif;
+  font-size: 12px;
+  line-height: 16px;
+
+  background-color: #3e85f3;
+  color: #fff;
+
+  padding: 8px 20px;
+  border-radius: 10px;
+  border: none;
+
+  text-align: center;
+  transition: background-color var(--cubic);
+  cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    line-height: 18px;
     
+    padding: 12px 32px;
+    border-radius: 14px;
+  }
+  &:hover,
+  &:focus {
+    background-color: #2b78ef;
+  }
 `;
