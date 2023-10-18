8c55084a28d9b8fd32f087a88ef24698f7115342
Diana Smilian <dianka.kulakevich@gmail.com>
2023-10-18 11:35:59 +0300

    почала налаштування бекенду

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,45 +1,58 @@
 import { useState, useRef } from 'react';
 import axios from 'axios';
-import UserPhoto from './UserPhoto'
-import { Input, Box, AccountForm, UserName, User, ButtonSubmit, InputBox, InputFile, Label } from './UserForm.styled';
-import { Formik, ErrorMessage, Field  } from 'formik';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs from 'dayjs';
-
-
+import UserPhoto from './UserPhoto';
+import {
+  Input,
+  Box,
+  AccountForm,
+  UserName,
+  User,
+  ButtonSubmit,
+  InputBox,
+  InputFile,
+  Label,
+} from './UserForm.styled';
+// import { CalendarGlobalStyles } from './Calendar.styled';
+import { Formik, ErrorMessage } from 'formik';
 import * as yup from 'yup';
+// import DatePicker from 'react-datepicker';
+// import "react-datepicker/dist/react-datepicker-cssmodules.css";
 
-const schema = yup.object().shape({
+
+const validationSchema = yup.object().shape({
   avatar: yup.mixed().test('fileType', (value) => {
     if (!value) return true;
-    const allowedExtensions = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'];
+    const allowedExtensions = [
+      'image/jpg',
+      'image/jpeg',
+      'image/png',
+      'image/webp',
+    ];
     return allowedExtensions.includes(value.type);
   }),
   name: yup.string().required(`Це обов'язкове поле`).max(16),
   email: yup.string().email().required(),
   birthday: yup.date().default(() => new Date()),
-  number: yup.string().matches(/^\+380\d{9}$/,  {  excludeEmptyString : true  }),
+  number: yup.string().matches(/^\+380\d{9}$/, { excludeEmptyString: true }),
   skype: yup.string().max(16),
 });
 
-const testAPI = 'https://65292a9855b137ddc83e4f49.mockapi.io/';
+const testAPI = 'https://goose-track-backend-deployment.onrender.com/';
 
 const UserForm = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [uploaded, setUploaded] = useState();
+  // const [birthdayChange, setBirthdayChange] = useState(new Date());
 
   const filePicker = useRef(null);
 
   const initialValue = {
     name: '',
     number: '',
-    birthday: dayjs(),
+    birthday: '',
     skype: '',
-    email: ''
-  }
-
-  
-
+    email: '',
+  };
 
   const handleClick = () => {
     filePicker.current.click();
@@ -52,7 +65,7 @@ const UserForm = () => {
   };
 
   const handleSubmit = async (value) => {
-  const {name, number, birthday, skype, email} = value;
+    const { name, number, birthday, skype, email } = value;
 
     const formData = new FormData();
     formData.append('avatar', uploaded);
@@ -68,89 +81,79 @@ const UserForm = () => {
     //   },
     // });
 
-    // const data = await res.data;
-// console.log(data)
-console.log(name)
-console.log(number)
-console.log(birthday)
+    const res = await axios.get(testAPI)
+    const data = await res.data;
+    console.log(data)
   };
 
   return (
     <Box>
-    <Formik onSubmit={handleSubmit} initialValues={initialValue} validationSchema={schema}>
-    <AccountForm>
-        
-      <UserPhoto onClick={handleClick} selectedImage={selectedImage}/>
-      <InputFile
-        type="file"
-        name='avatar'
-        onChange={handleChangePhoto}
-        ref={filePicker}
-        accept="image/*,.png,.ipg,.jpeg,.webp"
-      />
-      <UserName>User Name</UserName>
-      <User>User</User>
-      <InputBox>
-      <Label>
-        User Name
-        <Input  
-        type="text" 
-        name="name"
-        maxLength="16"
-        required
-        />
-        <ErrorMessage name='name'/>
-      </Label>
-      <Label>
-        Phone
-        <Input  
-        type="tel" 
-        name="number"
-        // pattern="/^(\+380\d{9})$/"
-        />
-        <ErrorMessage name='number'/>
-      </Label>
-      
-      <Label>
-        Birthday        
-        <Field
-    component={DatePicker}
-    selected={initialValue.birthday.toDate()}
-    name="birthday"
-    type='date'
-    className='calendar'
-    defaultValue={dayjs()}
-    format="YYYY/MM/DD"
-    // onChange={change}
-    // views={['year', 'month', 'day']}
-  />
-        <ErrorMessage name='birthday'/>
-      </Label>
-      <Label>
-        Skype
-        <Input  
-        type="text" 
-        name="skype"
-        />
-        <ErrorMessage name='skype'/>
+      <Formik
+        initialValues={initialValue}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <AccountForm>
+          <UserPhoto onClick={handleClick} selectedImage={selectedImage} />
+          <InputFile
+            type="file"
+            name="avatar"
+            onChange={handleChangePhoto}
+            ref={filePicker}
+            accept="image/*,.png,.ipg,.jpeg,.webp"
+          />
+          <UserName>User Name</UserName>
+          <User>User</User>
+          <InputBox>
+            <Label>
+              User Name
+              <Input type="text" name="name" maxLength="16" required />
+              <ErrorMessage name="name" />
+            </Label>
+            <Label>
+              Phone
+              <Input
+                type="tel"
+                name="number"
+                // pattern="/^(\+380\d{9})$/"
+              />
+              <ErrorMessage name="number" />
+            </Label>
 
-      </Label>
-      <Label>
-        Email
-        <Input  type="email" 
-        name="email"
-        pattern='([A-zА-я])+([0-9\-_\+\.])*([A-zА-я0-9\-_\+\.])*@([A-zА-я])+([0-9\-_\+\.])*([A-zА-я0-9\-_\+\.])*[\.]([A-zА-я])+'
-
-        />
-        <ErrorMessage name='email'/>
-
-      </Label>
-      </InputBox>
-      <ButtonSubmit type="submit">
-        Save Changes
-      </ButtonSubmit>
-    </AccountForm>
-    </Formik>
+            <Label>
+              Birthday
+              <Input
+                name="birthday"
+                type="date"
+                // className="calendar"
+                // selected={birthdayChange}
+                // onChange={(date) => {
+                //   setBirthdayChange(date);
+                // }}
+                // dateFormat={'yyyy MM dd'}
+                // calendarStartDay={1}
+              />
+              {/* <CalendarGlobalStyles/> */}
+              <ErrorMessage name="birthday" />
+            </Label>
+            <Label>
+              Skype
+              <Input type="text" name="skype" />
+              <ErrorMessage name="skype" />
+            </Label>
+            <Label>
+              Email
+              <Input
+                type="email"
+                name="email"
+                pattern="([A-zА-я])+([0-9\-_\+\.])*([A-zА-я0-9\-_\+\.])*@([A-zА-я])+([0-9\-_\+\.])*([A-zА-я0-9\-_\+\.])*[\.]([A-zА-я])+"
+              />
+              <ErrorMessage name="email" />
+            </Label>
+          </InputBox>
+          <ButtonSubmit type="submit">Save Changes</ButtonSubmit>
+        </AccountForm>
+      </Formik>
     </Box>
   );
 };
