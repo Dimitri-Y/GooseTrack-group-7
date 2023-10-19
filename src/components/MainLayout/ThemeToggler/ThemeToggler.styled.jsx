abeeda0ed49c3374a4bd62493d5a74d476e1b82d
Olexander Klymuk <115143348+swcAlexander@users.noreply.github.com>
2023-10-19 23:49:40 +0300

    add ThemeToggler (#48)

--- src/components/MainLayout/ThemeToggler/ThemeToggler.styled.jsx
+++ src/components/MainLayout/ThemeToggler/ThemeToggler.styled.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export const ThemeTogglerStyled = styled.button`
-  width: 24px;
-  height: 24px;
   fill: #3E85F3;
   border: none;
-  background-color: transparent;
-  outline: 1px solid skyblue;
+  display: flex;
+  justify content: center;
+  background-color: ${(props) =>
+    props.theme === 'light' ? '#F7F6F9' : '#171820'};
 `;
