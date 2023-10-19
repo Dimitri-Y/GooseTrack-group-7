2fddc901970d73db01c2d2b54a23158980077884
Kateryna1706 <119225233+Kateryna1706@users.noreply.github.com>
2023-10-19 14:03:19 +0200

    did base redux (#44)
    *added selectVisibleMessage

--- src/redux/tasks/tasksSelectors.js
+++ src/redux/tasks/tasksSelectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectTasks = (state) => state.tasks.items;
 
 export const selectIsLoading = (state) => state.tasks.isLoading;
@@ -6,4 +8,12 @@ export const selectError = (state) => state.tasks.error;
 
 export const selectFilter = (state) => state.filter.filter;
 
+export const selectVisibleMessage = createSelector([selectTasks], (tasks) => {
+  const hasUnfinishedTasks = tasks.some(
+    ({ category }) => category === 'to-do' || category === 'in-progress',
+  );
 
+  return hasUnfinishedTasks
+    ? 'Let go of the past and focus on the present!'
+    : '';
+});
