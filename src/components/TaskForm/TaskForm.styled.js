e9e2ee1d1a7911d2f85c058cd98d035102105a73
TarasMysiura <118620052+TarasMysiura@users.noreply.github.com>
2023-10-19 14:19:17 +0300

    Modal (#41)
    * add Modal.styled
    
    * del width modal
    
    * Update Modal.styled.js

--- src/components/TaskForm/TaskForm.styled.js
+++ src/components/TaskForm/TaskForm.styled.js
@@ -23,6 +23,7 @@ export const FormTask = styled.div`
   .label {
     display: flex;
     flex-direction: column;
+    font-family: 'Inter-500', sans-serif;
     /* gap: 2px; */
     /* width: 100%; */
     padding-bottom: 15px;
@@ -30,10 +31,10 @@ export const FormTask = styled.div`
   }
   .title {
     color: rgba(52, 52, 52, 0.8);
-    font-family: Inter;
+    font-family: 'Inter-500', sans-serif;
     font-size: 12px;
     font-style: normal;
-    font-weight: 500;
+    /* font-weight: 500; */
     line-height: 1.17;
     margin-bottom: 8px;
   }
@@ -85,38 +86,38 @@ export const FormTask = styled.div`
   .radioBtn_Wrapper {
     display: flex;
     gap: 16px;
-    
-  .custom-radio{
-    display: flex;
-    gap: 4px;
-    align-items: center
-  }
 
-  .custom-radio>input {
+    .custom-radio {
+      display: flex;
+      gap: 4px;
+      align-items: center;
+    }
+
+    .custom-radio > input {
       position: absolute;
       z-index: -1;
       opacity: 0;
     }
-    .custom-radio>.input_first{
-      background: #72C2F8
+    .custom-radio > .input_first {
+      background: #72c2f8;
     }
-    
+
     /* для элемента label связанного с label_input */
-    .custom-radio>span {
+    .custom-radio > span {
       display: inline-flex;
       align-items: center;
       user-select: none;
       color: #343434;
 
-font-family: Inter;
-font-size: 14px;
-font-style: normal;
-font-weight: 600;
-line-height: 18px; /* 128.571% */
-        }
+      font-family: 'Inter-600', sans-serif;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 18px; /* 128.571% */
+    }
 
     /* создание в label псевдоэлемента  before со следующими стилями */
-    .custom-radio>span::before {
+    .custom-radio > span::before {
       content: '';
       display: inline-block;
       width: 10px;
@@ -129,44 +130,41 @@ line-height: 18px; /* 128.571% */
       background-repeat: no-repeat;
       background-position: center center;
       background-size: 50% 50%;
-
-      
     }
 
     /* при наведенні курсора на радіо */
-    .custom-radio>input:not(:disabled):not(:checked)+span:hover::before {
+    .custom-radio > input:not(:disabled):not(:checked) + span:hover::before {
       border-color: #b3d7ff;
     }
 
     /* стили для активной радиокнопки (при нажатии на неё) */
-    .custom-radio>input:not(:disabled):active+span::before {
+    .custom-radio > input:not(:disabled):active + span::before {
       background-color: red;
       border-color: #b3d7ff;
     }
 
     /* стили для радиокнопки, находящейся в фокусе */
-    .custom-radio>input:focus+span::before {
+    .custom-radio > input:focus + span::before {
       box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
     }
 
     /* стили для радиокнопки, находящейся в фокусе и не находящейся в состоянии checked */
-    .custom-radio>input:focus:not(:checked)+span::before {
+    .custom-radio > input:focus:not(:checked) + span::before {
       border-color: #80bdff;
     }
 
     /* стили для радиокнопки, находящейся в состоянии checked */
-    .custom-radio>input:checked+span::before {
+    .custom-radio > input:checked + span::before {
       border-color: #0b76ef;
       background-color: #0b76ef;
       background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e");
     }
 
     /* стили для радиокнопки, находящейся в состоянии disabled */
-    .custom-radio>input:disabled+span::before {
+    .custom-radio > input:disabled + span::before {
       background-color: #e9ecef;
     }
   }
-
 `;
 // export const styleForm = styled.div`
 //   /* display: flex;
