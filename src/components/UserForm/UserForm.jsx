bdfdad86b56764e0fd1141dae7b23256ebfdc114
Dimitri-Y <dimoon.bond@gmail.com>
2023-10-21 00:25:39 +0300

    fix logic axios

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -18,7 +18,6 @@ import { Formik, ErrorMessage, Field } from 'formik';
 import * as yup from 'yup';
 import Calendar from './Calendar/Calendar';
 
-
 const validationSchema = yup.object().shape({
   avatar: yup.mixed().test('fileType', (value) => {
     if (!value) return true;
@@ -33,12 +32,20 @@ const validationSchema = yup.object().shape({
   name: yup.string().required(`Name is required`).max(16),
   email: yup.string().email().required(`Email is required`),
   birthday: yup.date().default(() => new Date()),
-  number: yup.string().matches(/^\+380\d{9}$/, 'The number should start with +380', { excludeEmptyString: true }),
+  number: yup
+    .string()
+    .matches(/^\+380\d{9}$/, 'The number should start with +380', {
+      excludeEmptyString: true,
+    }),
   skype: yup.string().max(16),
 });
 
 const API = 'http://localhost:3000/api/users/current';
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MzJjZDAwZDk4YjM0YzA4OGY2MjZhOCIsImlhdCI6MTY5NzgyODExNiwiZXhwIjoxNjk3OTEwOTE2fQ.jmyr2QML4aZ6ta9sHWMJzH3b5XUznKkyZM2T6ecBZMI";
+const API_PATCH = 'http://localhost:3000/api/users/edit';
+
+const TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MmZkMTVhODJmYTc5M2I4ZDZiNTNiMyIsImlhdCI6MTY5NzgzNTUxOSwiZXhwIjoxNjk3OTE4MzE5fQ.p4mivK7ySEng3YU-77zbNvv1OIvExBtMyOJHZrsLikQ';
+// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MzJjZDAwZDk4YjM0YzA4OGY2MjZhOCIsImlhdCI6MTY5NzgyODExNiwiZXhwIjoxNjk3OTEwOTE2fQ.jmyr2QML4aZ6ta9sHWMJzH3b5XUznKkyZM2T6ecBZMI';
 
 const UserForm = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -46,38 +53,40 @@ const UserForm = () => {
   const [birthdayChange, setBirthdayChange] = useState(new Date());
   const [userDate, setUserDate] = useState();
 
-  console.log(userDate)
+  console.log(userDate);
 
   const filePicker = useRef(null);
 
   // const user = useSelector(selectUser);
   // const dispatch = useDispatch();
   // console.log(user)
-  
+
   // useEffect(() => {
   //   dispatch(refreshUser());
   // }, [dispatch]);
 
   useEffect(() => {
-    axios.get(API, {
-      headers: {
-        'Authorization': `Bearer ${TOKEN}`,
-      },
-    }).then(response => {
-      const data = response.data;
-      setUserDate(data)
-    })
-    .catch(error => {
-      console.error('Помилка запиту:', error);
-    });
-  }, [birthdayChange])
+    axios
+      .get(API, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        setUserDate(data);
+      })
+      .catch((error) => {
+        console.error('Помилка запиту:', error);
+      });
+  }, []);
 
   const initialValue = {
-    name: userDate?.userName || "",
-    number: userDate?.phone || "",
+    name: userDate?.userName || '',
+    number: userDate?.phone || '',
     birthday: userDate?.birthday || new Date(),
-    skype: userDate?.skype || "",
-    email: userDate?.email || "",
+    skype: userDate?.skype || '',
+    email: userDate?.email || '',
   };
 
   const handleClick = () => {
@@ -90,34 +99,34 @@ const UserForm = () => {
     setSelectedImage(imgURL);
   };
 
-  const handleChangeDate = value => {
-    setBirthdayChange(value)
-  }
+  const handleChangeDate = (value) => {
+    setBirthdayChange(value);
+  };
 
   const handleSubmit = async (values) => {
     const { name, number, birthday, skype, email } = values;
 
     const formData = new FormData();
     formData.append('avatar', uploaded);
-    formData.append('name', name);
-    formData.append('number', number);
+    formData.append('userName', name);
+    formData.append('phone', number);
     formData.append('birthday', birthday);
     formData.append('skype', skype);
     formData.append('email', email);
+    const res = await axios.patch(API_PATCH, formData, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
 
-    // const res = await axios.post(API, formData, {
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data',
-    //   },
-    // });
-
-    // const data = await res.data;
-    console.log(birthday)
-    console.log(name)
-    console.log(number)
-    console.log(skype)
-    console.log(email)
-
+    const data = res.data;
+    console.log(data);
+    // console.log(birthday);
+    // console.log(name);
+    // console.log(number);
+    // console.log(skype);
+    // console.log(email);
   };
 
   return (
@@ -146,17 +155,14 @@ const UserForm = () => {
             </Label>
             <Label>
               Phone
-              <Input
-                type="tel"
-                name="number"
-              />
+              <Input type="tel" name="number" />
               <ErrorMessage name="number" />
             </Label>
 
             <Label>
               Birthday
               <Field
-              component={Calendar}
+                component={Calendar}
                 name="birthday"
                 type="date"
                 selected={birthdayChange}
@@ -165,17 +171,13 @@ const UserForm = () => {
               <ErrorMessage name="birthday" />
             </Label>
             <Label>
-              Skype              
-              
+              Skype
               <Input type="text" name="skype" />
               <ErrorMessage name="skype" />
             </Label>
             <Label>
               Email
-              <Input
-                type="email"
-                name="email"
-              />
+              <Input type="email" name="email" />
               <ErrorMessage name="email" />
             </Label>
           </InputBox>
