76e8b1c80f20783684a53e77e935d38822ca2f0f
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-10-22 00:00:40 +0300

    add images

---
+++ src/images/logo/index.js
@@ -0,0 +1,29 @@
+import desk1xPng from './logo-desk-1x.png';
+import desk1xWebp from './logo-desk-1x.webp';
+import desk2xPng from './logo-desk@2x.png';
+import desk2xWebp from './logo-desk@2x.webp';
+
+import tabl1xPng from './logo-tab-1x.png';
+import tabl1xWebp from './logo-tab-1x.webp';
+import tabl2xPng from './logo-tab@2x.png';
+import tabl2xWebp from './logo-tab@2x.webp';
+
+import mob1xPng from './logo-mob-1x.png';
+import mob1xWebp from './logo-mob-1x.webp';
+import mob2xPng from './logo-mob@2x.png';
+import mob2xWebp from './logo-mob@2x.webp';
+
+export const logo = {
+  desk1xPng,
+  desk1xWebp,
+  desk2xPng,
+  desk2xWebp,
+  tabl1xPng,
+  tabl1xWebp,
+  tabl2xPng,
+  tabl2xWebp,
+  mob1xPng,
+  mob1xWebp,
+  mob2xPng,
+  mob2xWebp,
+};
