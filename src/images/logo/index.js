3fe3f213f3a83b3731f69c60ed99d1f0b12bd736
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-10-22 01:57:38 +0300

    add images

--- src/images/logo/index.js
+++ src/images/logo/index.js
@@ -3,10 +3,10 @@ import desk1xWebp from './logo-desk-1x.webp';
 import desk2xPng from './logo-desk@2x.png';
 import desk2xWebp from './logo-desk@2x.webp';
 
-import tabl1xPng from './logo-tab-1x.png';
-import tabl1xWebp from './logo-tab-1x.webp';
-import tabl2xPng from './logo-tab@2x.png';
-import tabl2xWebp from './logo-tab@2x.webp';
+import tab1xPng from './logo-tab-1x.png';
+import tab1xWebp from './logo-tab-1x.webp';
+import tab2xPng from './logo-tab@2x.png';
+import tab2xWebp from './logo-tab@2x.webp';
 
 import mob1xPng from './logo-mob-1x.png';
 import mob1xWebp from './logo-mob-1x.webp';
@@ -18,10 +18,10 @@ export const logo = {
   desk1xWebp,
   desk2xPng,
   desk2xWebp,
-  tabl1xPng,
-  tabl1xWebp,
-  tabl2xPng,
-  tabl2xWebp,
+  tab1xPng,
+  tab1xWebp,
+  tab2xPng,
+  tab2xWebp,
   mob1xPng,
   mob1xWebp,
   mob2xPng,
