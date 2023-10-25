69f868d048fa3236717bbdebf8199e385da3da14
9Alla9 <konfetcka@gmail.com>
2023-10-26 01:01:30 +0300

    доопрацювала код

--- src/components/ChoosedMonth/ChoosedMonth.jsx
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -1,26 +1,18 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
-import { selectTasks } from '../../redux/tasks/tasksSelectors';
-import { fetchTasks } from '../../redux/tasks/tasksOperations';
+import { selectDateCalendar } from '../../redux/tasks/tasksSelectors';
 import CalendarTable from './CalendarTable/CalendarTable';
 import { MonthCalendarHeader } from './MonthCalendarHeader/MonthCalendarHeader';
 
 const ChoosedMonth = () => {
-  const dispatch = useDispatch();
-
-  const tasks = useSelector(selectTasks);
+  const tasks = useSelector(selectDateCalendar);
 
   moment.updateLocale('en', { week: { dow: 1 } });
 
   const { currentDate } = useParams();
   const today = moment(currentDate);
   const startDay = today.clone().startOf('month').startOf('week');
-
-  useEffect(() => {
-    dispatch(fetchTasks(currentDate));
-  }, [currentDate, dispatch]);
 
   return (
     <>
