6da76cfc13f97595722e63fb36158bd44830b757
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-10-25 08:37:52 +0300

    Feature current page (#101)
    * Add retina img
    
    * Add show current page and scrolling

--- src/images/logo/index.js
+++ src/images/logo/index.js
@@ -1,29 +1,41 @@
-import desk1xPng from './logo-desk-1x.png';
-import desk1xWebp from './logo-desk-1x.webp';
+import desk1xPng from './logo-desk@1x.png';
+import desk1xWebp from './logo-desk@1x.webp';
 import desk2xPng from './logo-desk@2x.png';
 import desk2xWebp from './logo-desk@2x.webp';
+import desk3xPng from './logo-desk@3x.png';
+import desk3xWebp from './logo-desk@3x.webp';
 
-import tab1xPng from './logo-tab-1x.png';
-import tab1xWebp from './logo-tab-1x.webp';
+import tab1xPng from './logo-tab@1x.png';
+import tab1xWebp from './logo-tab@1x.webp';
 import tab2xPng from './logo-tab@2x.png';
 import tab2xWebp from './logo-tab@2x.webp';
+import tab3xPng from './logo-tab@3x.png';
+import tab3xWebp from './logo-tab@3x.webp';
 
-import mob1xPng from './logo-mob-1x.png';
-import mob1xWebp from './logo-mob-1x.webp';
+import mob1xPng from './logo-mob@1x.png';
+import mob1xWebp from './logo-mob@1x.webp';
 import mob2xPng from './logo-mob@2x.png';
 import mob2xWebp from './logo-mob@2x.webp';
+import mob3xPng from './logo-mob@3x.png';
+import mob3xWebp from './logo-mob@3x.webp';
 
 export const logo = {
   desk1xPng,
   desk1xWebp,
   desk2xPng,
   desk2xWebp,
+  desk3xPng,
+  desk3xWebp,
   tab1xPng,
   tab1xWebp,
   tab2xPng,
   tab2xWebp,
+  tab3xPng,
+  tab3xWebp,
   mob1xPng,
   mob1xWebp,
   mob2xPng,
   mob2xWebp,
+  mob3xPng,
+  mob3xWebp,
 };
