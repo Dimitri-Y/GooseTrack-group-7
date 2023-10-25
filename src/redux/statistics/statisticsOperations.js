89527f64b0ad78ad7cc7b241cfdc018407ae5e90
Dimitri-Y <dimoon.bond@gmail.com>
2023-10-25 08:21:59 +0300

    Update statisticsOperations.js

--- src/redux/statistics/statisticsOperations.js
+++ src/redux/statistics/statisticsOperations.js
@@ -4,14 +4,19 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL =
   'https://goose-track-backend-deployment-q70i.onrender.com/api/';
 
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
 export const fetchStatisticsDataThunk = createAsyncThunk(
   'statistics/fetchAll',
-    async (dateFetchData, thunkAPI) => {
-        // axios.defaults.baseURL =  'http://localhost:3000/api/'
-     
+  async (dateFetchData, thunkAPI) => {
+    // axios.defaults.baseURL =  'http://localhost:3000/api/'
+
     try {
-        const response = await axios.get(`/statistics/${dateFetchData}`);       
-        return response.data;
+      const response = await axios.get(`/statistics/${dateFetchData}`);
+      setAuthHeader(response.data.token);
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
