d47fa7e9a4a4c8332d3483853c26e3452d4b0ada
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2023-10-25 14:00:16 +0300

    bug fix err401

--- src/redux/statistics/statisticsOperations.js
+++ src/redux/statistics/statisticsOperations.js
@@ -4,18 +4,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL =
   'https://goose-track-backend-deployment-q70i.onrender.com/api/';
 
-const setAuthHeader = (token) => {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
 
 export const fetchStatisticsDataThunk = createAsyncThunk(
   'statistics/fetchAll',
   async (dateFetchData, thunkAPI) => {
-    // axios.defaults.baseURL =  'http://localhost:3000/api/'
 
     try {
-      const response = await axios.get(`/statistics/${dateFetchData}`);
-      setAuthHeader(response.data.token);
+      const response = await axios.get(`/statistics/${dateFetchData}`);y
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
