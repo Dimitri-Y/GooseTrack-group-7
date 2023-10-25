e5ac3fd6ecb42276c073098e4cd5c729f7a34d4e
Kateryna1706 <silckatya@gmail.com>
2023-10-25 18:44:16 +0200

    added styles adaptive

--- src/components/CalendarToolbar/CalendarToolbar.styled.js
+++ src/components/CalendarToolbar/CalendarToolbar.styled.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const CalendarToolbarStyles = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 18px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
